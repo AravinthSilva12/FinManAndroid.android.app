49dab8205fa91c397f30a47961591b860577b938
Aravinthasamy <aravinthasamy281099@gmail.com>
2026-04-22 18:35:39 +0530

    April 11 : project setup - packages established; Navigation is half finished. April 12 : BottomNavigationBar is completed. April 13 : Navigation architecture is completed. Bottom Nav bar and screen switching is working good now. April 16 : Hilt dependencies added in gradle. April 22 : Data base design in progress*

--- app/src/main/java/com/aravinth/financemanager/data/di/DatabaseModule.kt
+++ app/src/main/java/com/aravinth/financemanager/data/di/DatabaseModule.kt
@@ -1,13 +1,33 @@
 package com.aravinth.financemanager.data.di
 
+import android.content.Context
+import androidx.room.Room
+import com.aravinth.financemanager.data.local.AccountingDao
+import com.aravinth.financemanager.data.local.AppDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "finance_db"
+        ).build()
+    }
 
+    @Provides
+    fun provideAccountingDao(db: AppDatabase): AccountingDao {
+        return db.accountingDao()
+    }
 }
