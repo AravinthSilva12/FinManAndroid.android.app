bfed8ae860ca976ef8329f41626e51189b4c4fe3
Aravinthasamy <aravinthasamy281099@gmail.com>
2026-05-16 18:55:48 +0530

    April 11 : project setup - packages established; Navigation is half finished. April 12 : BottomNavigationBar is completed. April 13 : Navigation architecture is completed. Bottom Nav bar and screen switching is working good now. April 16 : Hilt dependencies added in gradle. April 22 : Data base design in progress* April 24 : View model in progress* April 26 : Front end in progress* May 1 : model, entity and mapper class created* May 2 : Model data layer is in progress* May 3 : Model is continued, usecase in progress* May 4 : data package in progress* May 6 : data layer* May 10 : data layer for accounting module is finished* May 11 : UI in progress* May 14 : Accounting screens in progress* May 16 : Add transaction in progress* ViewTrn and AddTrn buttons are working but need proper UI fix*

--- app/src/main/java/com/aravinth/financemanager/ui/screen/accounting/AccountingScreen.kt
+++ app/src/main/java/com/aravinth/financemanager/ui/screen/accounting/AccountingScreen.kt
@@ -1,10 +1,17 @@
 package com.aravinth.financemanager.ui.screen.accounting
 
+import android.R.attr.icon
+import android.R.attr.onClick
+import android.R.attr.text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -13,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.aravinth.financemanager.ui.navigation.Screen
@@ -26,11 +34,24 @@ fun AccountingScreen (navController: NavController, viewModel: AccountingViewMod
 
     //Scaffold:
     Scaffold(modifier = Modifier.fillMaxSize(),
-        floatingActionButton = { ExtendedFloatingActionButton(onClick = {navController.navigate(Screen.AddTransaction) },
-        icon = { Icon(Icons.Default.Add, contentDescription = null) },
-        text = { Text("Add Transaction") } )
+        floatingActionButton = {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+
+                ExtendedFloatingActionButton(
+                    onClick = { navController.navigate(Screen.ViewTransactions) },
+                    icon = { Icon(imageVector = Icons.Default.Visibility, contentDescription = null) },
+                    text = { Text("View Transactions") }
+                )
+
+                ExtendedFloatingActionButton(
+                    onClick = { navController.navigate(Screen.AddTransaction) },
+                    icon = { Icon(Icons.Default.Add, contentDescription = null) },
+                    text = { Text("Add Transaction") }
+                )
+            }
         }
-    ) {it ->
+    )
+    {it ->
                 LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
 
                 }
--- app/src/main/java/com/aravinth/financemanager/ui/screen/accounting/ViewTransactions.kt
+++ app/src/main/java/com/aravinth/financemanager/ui/screen/accounting/ViewTransactions.kt
@@ -1,10 +1,21 @@
 package com.aravinth.financemanager.ui.screen.accounting
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.aravinth.financemanager.viewmodel.AccountingViewModel
 
 @Composable
-fun ViewTransactions(navController: NavController) {
-
-
+fun ViewTransactions(navController: NavController,
+                     viewModel: AccountingViewModel = viewModel()) {
+     Box(modifier =  Modifier.fillMaxSize().padding(4.dp)){
+         Text("Under construction!", textAlign = TextAlign.Center)
+     }
 }
