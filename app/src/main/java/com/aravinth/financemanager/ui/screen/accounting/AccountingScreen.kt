1434e5084769f96bf826f6172c5b757391f6c213
Aravinthasamy <aravinthasamy281099@gmail.com>
2026-05-23 13:32:16 +0530

    April 11 : project setup - packages established; Navigation is half finished. April 12 : BottomNavigationBar is completed. April 13 : Navigation architecture is completed. Bottom Nav bar and screen switching is working good now. April 16 : Hilt dependencies added in gradle. April 22 : Data base design in progress* April 24 : View model in progress* April 26 : Front end in progress* May 1 : model, entity and mapper class created* May 2 : Model data layer is in progress* May 3 : Model is continued, usecase in progress* May 4 : data package in progress* May 6 : data layer* May 10 : data layer for accounting module is finished* May 11 : UI in progress* May 14 : Accounting screens in progress* May 16 : Add transaction in progress* ViewTrn and AddTrn buttons are working but need proper UI fix* May 19 : UI for final version of accounting module in progress* May 23 : Addtransac, JournalScr, LedScr, FinRepScr are pending*. Focusing on Addtransaction screen today.

--- app/src/main/java/com/aravinth/financemanager/ui/screen/accounting/AccountingScreen.kt
+++ app/src/main/java/com/aravinth/financemanager/ui/screen/accounting/AccountingScreen.kt
@@ -1,19 +1,38 @@
 package com.aravinth.financemanager.ui.screen.accounting
 
+import android.graphics.Paint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.aravinth.financemanager.ui.navigation.Screen
 import com.aravinth.financemanager.viewmodel.AccountingViewModel
@@ -34,11 +53,56 @@ fun AccountingScreen (navController: NavController, viewModel: AccountingViewMod
                 )
         }
     )
-    {it ->
-                LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
+    {
+        Column(modifier = Modifier.fillMaxSize().padding(it)) {
 
+                Spacer(modifier = Modifier.height(8.dp))
+
+            Row(modifier = Modifier.fillMaxWidth().padding(2.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly) {
+                TextButton(onClick = { navController.navigate(Screen.Journal)}) {
+                    Text("Journal")
                 }
-       }
+
+                TextButton(onClick = { navController.navigate(Screen.Ledger)}) {
+                    Text("Ledger")
+                }
+
+                TextButton(onClick = { navController.navigate(Screen.FinancialReport) }) {
+                    Text("Financial report")
+                }
+            }
+                Spacer(modifier = Modifier.height(16.dp))
+
+            Card(modifier = Modifier.fillMaxWidth().height(150.dp).padding(horizontal = 8.dp)) {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                     Text("Dash board space")
+                }
+            }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+            LazyColumn(modifier = Modifier.fillMaxWidth().weight(1f).padding(horizontal = 8.dp),
+                contentPadding = PaddingValues(bottom = 80.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                    item{
+                        Text("Recent transactions:", fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+                    }
+                items(transactions){singeTraction ->
+                    Card(modifier = Modifier.fillMaxWidth()
+                        .padding(horizontal = 4.dp)
+                        .clickable(onClick = {navController.navigate(Screen.Journal)})
+                    ) {
+                        Box(modifier = Modifier.fillMaxWidth()
+                            .padding(16.dp)
+                        ) {
+                            Text(text = "Amount: ${singeTraction.amount}",
+                                style = MaterialTheme.typography.bodyLarge)
+                        }
+                    }
+                }
+            }
+        }
     }
+}
 
 
