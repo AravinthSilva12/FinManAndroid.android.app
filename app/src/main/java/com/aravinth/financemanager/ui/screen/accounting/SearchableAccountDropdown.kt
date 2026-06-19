e62b7f93aa1a9f035971d68da59d06d2f110e946
Aravinthasamy <aravinthasamy281099@gmail.com>
2026-06-19 22:40:45 +0530

    April 11 : project setup - packages established; Navigation is half finished. April 12 : BottomNavigationBar is completed. April 13 : Navigation architecture is completed. Bottom Nav bar and screen switching is working good now. April 16 : Hilt dependencies added in gradle. April 22 : Data base design in progress* April 24 : View model in progress* April 26 : Front end in progress* May 1 : model, entity and mapper class created* May 2 : Model data layer is in progress* May 3 : Model is continued, usecase in progress* May 4 : data package in progress* May 6 : data layer* May 10 : data layer for accounting module is finished* May 11 : UI in progress* May 14 : Accounting screens in progress* May 16 : Add transaction in progress* ViewTrn and AddTrn buttons are working but need proper UI fix* May 19 : UI for final version of accounting module in progress* May 23 : Addtransac, JournalScr, LedScr, FinRepScr are pending*. Focusing on Addtransaction screen today. May 24 : AddTransaction screen is in progress* June 01 : AddTransaction screen in progress - Debit & credit side state in progress* June 02 : Dropdown screen in progress* June 19 : just revisiting*

---
+++ app/src/main/java/com/aravinth/financemanager/ui/screen/accounting/SearchableAccountDropdown.kt
@@ -0,0 +1,52 @@
+package com.aravinth.financemanager.ui.screen.accounting
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchableAccountDropdown(
+     accountsList: List<String>,
+     selectedAccount: String,
+     searchQuery: String,
+     onAccountSelectChange: (String) -> Unit,
+     onSearchQueryChange: (String) -> Unit,
+     onAddNewAccountClick: () -> Unit,
+     label: String,
+     modifier: Modifier = Modifier
+){
+    var expanded by remember { mutableStateOf(false) }
+    val filteredAccounts = accountsList.filter {
+        it.contains(searchQuery, ignoreCase = true) }
+
+    ExposedDropdownMenuBox(expanded = expanded,
+        onExpandedChange = {expanded = it})
+    {
+        OutlinedTextField(
+            value = selectedAccount,
+            onValueChange = {},
+            readOnly = true,
+            label = {Text(label)},
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)},
+            colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
+            modifier = Modifier.fillMaxWidth().menuAnchor()
+            )
+
+        ExposedDropdownMenu(expanded = expanded,
+            onDismissRequest = {expanded = false})
+        {
+
+
+        }
+    }
+}
